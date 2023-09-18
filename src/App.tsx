173c9f783a3fbef1df8acec9379289fe46fb948c
indrasetiadhi <indrasetiadhi4@gmail.com>
2023-09-18 23:37:50 +0700

    style: remove comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,9 @@
-import { useState, useEffect } from "react";
-//import reactLogo from "./assets/react.svg";
-//import viteLogo from "/vite.svg";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { PokemonDetail } from "./components/PokemonDetail";
 import { SearchBar } from "./components/SearchBar";
 
 function App() {
-  // const [pokemons, setPokemons] = useState<Pokemon[]>([]);
-
-  // async function fetchPokemons() {
-  //   try {
-  //     const response = await axios.get(
-  //       "https://pokeapi.co/api/v2/pokemon?limit=1500"
-  //     );
-  //     console.log(response.data.results);
-  //     setPokemons(response.data.results);
-  //   } catch (error) {
-  //     console.error("Error fetching pokemons:", error);
-  //     throw error;
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   fetchPokemons();
-  // }, []);
-
   return (
     <BrowserRouter>
       <Routes>
